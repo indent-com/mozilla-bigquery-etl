6d60a162fec64956c0a83ef717b00c444b094588
Chris DeCarolis <chris@indent.com>
2026-03-04 19:26:43 +0000

    Add uniqueness check for ssl_ratios_v1 to prevent duplicate rows.
    Generated with [Indent](https://indent.com)
    Co-Authored-By: Indent <noreply@indent.com>

--- sql/moz-fx-data-shared-prod/telemetry_derived/ssl_ratios_v1/checks.sql
+++ sql/moz-fx-data-shared-prod/telemetry_derived/ssl_ratios_v1/checks.sql
@@ -5,4 +5,7 @@
 {{ min_row_count(1, "submission_date = @submission_date") }}
 
 #fail
+{{ is_unique(["submission_date", "os", "country"], "submission_date = @submission_date") }}
+
+#fail
 {{ in_range(["non_ssl_loads", "ssl_loads", "reporting_ratio"], 0, none, "submission_date = @submission_date") }}
